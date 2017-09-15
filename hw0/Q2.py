1079b03c99b37de4ed9458941af554e09f21b264
r06945003 <r06945003@ntu.edu.tw>
2017-09-15 15:50:57 +0800

    add file

---
+++ hw0/Q2.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import sys
+#import numpy
+im = Image.open(sys.argv[1] )
+#im = Image.open('westbrook.jpg' )
+#one = numpy.array(im)
+#print (one[0])
+x, y = im.size
+for m in range(x):
+    for n in range(y):
+        RGB = im.getpixel((m,n))
+        r = RGB[0] // 2
+        g = RGB[1] // 2
+        b = RGB[2] // 2
+        im.putpixel((m,n), (r, g, b))
+im.save("Q2.jpg")
+#im2 = Image.open('Q2.jpg')
+#two = numpy.array(im)
+#print (two[0])
